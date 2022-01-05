48df43f257ee3e8f292a548bf84224c6007cb14e
NoOne125 <chebotok12345@gmail.com>
2022-01-05 19:40:11 +0200

    ке

--- lab2.js
+++ lab2.js
@@ -49,7 +49,7 @@ function hasCookies() {
 			let eqPos = cookie.indexOf("=");
 			let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
 			document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
-			document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+			document.cookie = name + "=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 		}
 	} else {
 		alert("Cookies were saved");
